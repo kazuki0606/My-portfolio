2fbf6e4eb62821f07294037f276c2e09ac927088
kazuki0606 <fkazuki666@icloud.com>
2020-09-01 11:46:54 +0900

    a

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+
+$(function(){
+
+    $('.sp-menu').click(function(){
+        $('.han').toggleClass('is-active');
+      });
+      
+
+ 
+ 
+
+
+
+
+
+  
+  var effect_btm = 300; // 画面下からどの位置でフェードさせるか(px)
+  var effect_move = 50; // どのぐらい要素を動かすか(px)
+  var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+
+
+  
+
+  //親要素と子要素のcssを定義
+  $('.scroll-fade-row').css({
+      opacity: 0
+  });
+  $('.scroll-fade-row').children().each(function(){
+      $(this).css({
+          opacity: 0,
+          transform: 'translateY('+ effect_move +'px)',
+          transition: effect_time + 'ms'
+      });
+  });
+
+  // スクロールまたはロードするたびに実行
+  $(window).on('scroll load', function(){
+      var scroll_top = $(this).scrollTop();
+      var scroll_btm = scroll_top + $(this).height();
+      var effect_pos = scroll_btm - effect_btm;
+
+      //エフェクトが発動したとき、子要素をずらしてフェードさせる
+      $('.scroll-fade-row').each( function() {
+          var this_pos = $(this).offset().top;
+          if ( effect_pos > this_pos ) {
+              $(this).css({
+                  opacity: 1,
+                  transform: 'translateY(0)'
+              });
+              $(this).children().each(function(i){
+                  $(this).delay(100 + i*200).queue(function(){
+                      $(this).css({
+                          opacity: 1,
+                          transform: 'translateY(0)'
+                      }).dequeue();
+                  });
+              });
+          }
+      });
+
+
+      load_effect();
+      $(window).scroll(function (){
+          scroll_effect();
+      });  
+  //ふわっとロード
+  function load_effect(){
+    var tt = $(window).scrollTop();
+    var hh = $(window).height();
+    $('.load-fade').each(function(){
+        var yy = $(this).offset().top;
+        if (tt > yy - hh){
+            $(this).addClass('done');
+        }
+    });
+  
+}
+
+  });
+
+  
+  
+
+});
+
+
+
+
+
+   
+
+ 
+
+ 
